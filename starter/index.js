2221288a3969f85ee09c9fd5488211c1f2b95d3d
unicorn453 <kira.ruseva@gmail.com>
2024-02-17 20:34:28 +0200

    added questions array

--- starter/index.js
+++ starter/index.js
@@ -4,7 +4,63 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter installation instructions:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter usage information:",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Enter contribution guidelines:",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Enter test instructions:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your application:",
+    choices: [
+      "MIT License",
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+      "GNU General Public License v2.0",
+      "BSD 2-Clause Simplified License",
+      "BSD 3-Clause New or Revised License",
+      "Mozilla Public License 2.0 (MPL-2.0)",
+      "GNU Affero General Public License v3.0 (AGPL-3.0)",
+    ],
+  },
+  {
+    type: "input",
+    name: "GitHubName",
+    message: "Enter your GitHub username:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email:",
+  },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {}
@@ -19,7 +75,7 @@ function init() {
       const readmeContent = generateMarkdown(answers);
 
       // Write README file
-      writeToFile("README.md", readmeContent);
+      writeToFile("README1.md", readmeContent);
     })
     .catch((err) => {
       console.error("Error occurred:", err);
