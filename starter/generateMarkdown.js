ba6ef7da2832ae67366cc9ffb6c50db93c861afb
unicorn453 <kira.ruseva@gmail.com>
2024-02-20 12:47:19 +0200

    added feature for a license badge

--- starter/generateMarkdown.js
+++ starter/generateMarkdown.js
@@ -1,6 +1,25 @@
 function generateMarkdown(answers) {
+  const licenseBadges = {
+    "MIT License":
+      "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)",
+    "Apache License 2.0":
+      "![License: Apache-2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)",
+    "GNU General Public License v3.0":
+      "![License: GPL-3.0](https://img.shields.io/badge/License-GPLv3-blue.svg)",
+    "GNU General Public License v2.0":
+      "![License: GPL-2.0](https://img.shields.io/badge/License-GPL%202.0-blue.svg)",
+    "BSD 2-Clause Simplified License":
+      "![License: BSD-2-Clause](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)",
+    "BSD 3-Clause New or Revised License":
+      "![License: BSD-3-Clause](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)",
+    "Mozilla Public License 2.0 (MPL-2.0)":
+      "![License: MPL-2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)",
+    "GNU Affero General Public License v3.0 (AGPL-3.0)":
+      "![License: AGPL-3.0](https://img.shields.io/badge/License-AGPL%203.0-blue.svg)",
+  };
+  const licenseBadge = licenseBadges[answers.license];
   return `
-# ${answers.title}
+# ${answers.title} ${licenseBadge}
 
 ## Description
 ${answers.description}
